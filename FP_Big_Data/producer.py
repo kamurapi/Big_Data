55c308e840ffd820f3aa11549960b9615190b691
Dimas Kamurapi <dkamurapi@gmail.com>
2019-05-23 21:04:26 +0700

    default consumer and producer

---
+++ FP_Big_Data/producer.py
@@ -0,0 +1,16 @@
+from time import sleep
+from json import dumps
+from kafka import KafkaProducer
+import os
+import pandas as pd
+
+
+producer = KafkaProducer(bootstrap_servers=['localhost:9092'],
+                         value_serializer=lambda x: 
+                         dumps(x).encode('utf-8'))
+
+
+for e in range(1000):
+    data = {'number' : e}
+    producer.send('numtest', value=data)
+    sleep(5)
